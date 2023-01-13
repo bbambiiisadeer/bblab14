6f6e0ac2b22213151dcc193bd1a73196f1ff8eba
bbam <nunnapats1822@gmail.comr>
2023-01-13 15:50:26 +0700

    bblab14_1completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,41 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0;
+    for(int i = 0; i < N; i++) sum += A[i];
+        B[0] = sum/N;
+        
+    double x = 0;
+    for(int i = 0; i < N; i++) x += pow(A[i]-B[0],2);
+        B[1] = sqrt(x/N);
+        
+    double y = 1;
+    for(int i = 0; i < N; i++) y *= A[i]; 
+        B[2] = pow(y,1.0/N);
+        
+    double z = 0;
+    for(int i = 0; i < N; i++) z += 1.0/A[i]; 
+        B[3] = N/z;
+    
+    int i = 0;
+    double w = A[0];
+        while(i < N){
+            if(A[i] > w){
+                w = A[i];
+            }
+        i++;
+        }
+        B[4] = w;
+        
+    i = 0;  
+    double v = A[0];
+        while(i < N){
+            if(A[i] < v){
+                v = A[i];
+            }   
+        i++;
+        }
+        B[5] = v;    
+}
